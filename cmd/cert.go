simonoff/gitea

Add CommonName to the self signed certificate
Without a CN the self signed certificate is considered
invalid by chrome.
You can check with: openssl x509 -in cert.pem -subject -noout

cmd/cert.go
@@ -114,6 +114,7 @@ func runCert(ctx *cli.Context) {
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
 			Organization: []string{"Acme Co"},
+			CommonName: "Gitea",
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,

smartech7/go_gitcha

cmd/cert.go
@@ -114,6 +114,7 @@ func runCert(ctx *cli.Context) {
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
 			Organization: []string{"Acme Co"},
+			CommonName: "Gogs",
 		},
 		NotBefore: notBefore,
 		NotAfter:  notAfter,